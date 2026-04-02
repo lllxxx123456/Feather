b50482c10de81e5af15637d75618ff3df77312a3
lllxxx123456 <luorixx@qq.com>
2026-04-03 01:43:31 +0800

    Add files via upload

---
+++ Feather/Extensions/CoreData+Sendable.swift
@@ -0,0 +1,11 @@
+//
+//  CoreData+Sendable.swift
+//  Feather778
+//
+
+import CoreData
+
+extension AltSource: @unchecked Sendable {}
+extension CertificatePair: @unchecked Sendable {}
+extension Signed: @unchecked Sendable {}
+extension Imported: @unchecked Sendable {}
